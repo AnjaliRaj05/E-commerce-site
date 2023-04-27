a2629d0e5041814e7b16363815426f375b280e3e
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-04-28 01:11:44 +0530

    seven

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ import Login from './Component/LoginPage/Login';
 import Rejistration from './Component/RejistrationForm/Rejistration';
 import Contact from './Component/Contact';
 import About from './Component/About';
+import Navbar from './Component/NavSection/Navbar';
+import Cart from './Component/MyCart/Cart'
+//import Checkout from './Component/MyCart/CheckOut';
+import CheckOut from './Component/MyCart/CheckOut';
+//import Products from './Component/ProductSection/Products';
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Routes>
 <Route exact path="/" element={<Home/>}/>
 <Route exact path="/products" element={<Products/>}/>
 <Route exact path="/products/:id" element={<Product/>}/>
 <Route exact path="/contact" element={<Contact/>}/>
+<Route exact path="/cart" element={<Cart/>}/>
+<Route exact path="/checkout" element={<CheckOut/>}/>
 <Route exact path="/about" element={<About/>}/>
 <Route exact path="/login" element={<Login/>}/>
 <Route exact path="/rejistration" element={<Rejistration/>}/>
--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import {BrowserRouter} from 'react-router-dom';
+ import { Provider } from 'react-redux';
+  import store from './redux/store';
+
 
 // import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
+   <Provider store={store}> 
+ 
     <App />
-   
-  </React.StrictMode>
+    </Provider> 
+  
    </BrowserRouter>
 );
 
